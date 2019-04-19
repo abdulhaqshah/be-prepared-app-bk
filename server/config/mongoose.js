f29d9ed34610ea3590254d5f7568a36831317ee0
Haris1994 <billichuha@hotmail.com>
2019-04-19 15:30:24 +0500

    Made the changes for the connection of mongodb to call it through environment

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://localhost:27017/BePreparedApp');
 
 module.exports = {mongoose};
 
