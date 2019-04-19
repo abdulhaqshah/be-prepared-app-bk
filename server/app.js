846acc4d69e79f85ec8269c2979a9205319bf99a
Haris1994 <billichuha@hotmail.com>
2019-04-19 12:33:54 +0500

    Made the changes for the config folder for the PR#1

--- server/app.js
+++ server/app.js
@@ -1,18 +1,8 @@
-const path = require('path');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const _ = require('lodash');
-// const fs = require('fs');
 const usersRouter = require('./routes/user');
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/BePreparedApp');
-// const {User} = require('./DataBase/models/user');
-// const {Course} = require('./DataBase/models/course');
-
-const publicPath = path.join(__dirname , '../public');
-const port = process.env.PORT || 8080;
+const {port, publicPath} = require('./config/port');
 
 
 const app = express();
