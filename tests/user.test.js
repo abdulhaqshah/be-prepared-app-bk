0481a338d9c0f91791de5131307914ae9a98aa3c
Haris1994 <billichuha@hotmail.com>
2019-04-24 13:21:50 +0500

    Written tests for user update functionality and made some changes in the user update functionality

--- tests/user.test.js
+++ tests/user.test.js
@@ -110,3 +110,126 @@ describe('POST /user/login', () => {
         });
     });
 });
+
+describe('PATCH /user/userUpdate', () => {
+
+    it('should update the user', (done) => {
+        let body = {
+            id : users[0]._id,
+            image : 'folder/image.png',
+            name : 'Asad',
+            email : 'asad@example.com',
+            progress : []
+        }
+
+        request(app)
+        .patch('/user/userUpdate')
+        .send(body)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.data._id).toBeTruthy();
+            expect(res.body.data.image).toBe(body.image);
+            expect(res.body.data.name).toBe(body.name);
+            expect(res.body.data.email).toBe(body.email);
+        })
+        .end((error) => {
+            if(error){
+                done(error);
+            }
+            done();
+        })
+    })
+
+    it('should update the user when credentials are given by user to change', (done) => {
+        let id = users[0]._id;
+        let name = 'Asad';
+        let email = 'asad@example.com';
+
+        request(app)
+        .patch('/user/userUpdate')
+        .send({id,name,email})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.data._id).toBeTruthy();
+            expect(res.body.data.name).toBe(name);
+            expect(res.body.data.email).toBe(email);
+        })
+        .end((error) => {
+            if(error){
+                done(error);
+            }
+            done();
+        })
+    })
+
+    it('should update the user with the changes provided given that user id is valid and existing', (done) => {
+        let id = users[0]._id;
+        let name = 'Asaad';
+
+        request(app)
+        .patch('/user/userUpdate')
+        .send({id,name})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.data._id).toBeTruthy();
+            expect(res.body.data.name).toBe(name);
+        })
+        .end((error) => {
+            if(error){
+                done(error);
+            }
+            done();
+        })
+    })
+
+    it('should reject update if name is empty' , (done) =>{
+        let id = users[1]._id;
+        let name = '';
+        let email = 'asad@example.com';
+
+        request(app)
+        .patch('/user/userUpdate')
+        .send({id,name,email})
+        .expect(403)
+        .end((err,res) => {
+            if(err){
+                return done(err);
+            }
+            done(); 
+        });
+    })
+
+    it('should reject update if id is invalid or not existing' , (done) =>{
+        let id = users[1]._id + 1;
+        let email = 'asad@example.com';
+
+        request(app)
+        .patch('/user/userUpdate')
+        .send({id,email})
+        .expect(404)
+        .end((err,res) => {
+            if(err){
+                return done(err);
+            }
+            expect(res.text).toBe('User has not been found');
+            done(); 
+        });
+    })
+
+    it('should reject update if email is invalid' , (done) =>{
+        let id = users[1]._id;
+        let email = 'asad';
+
+        request(app)
+        .patch('/user/userUpdate')
+        .send({id,email})
+        .expect(403)
+        .end((err,res) => {
+            if(err){
+                return done(err);
+            }
+            expect(res.text).toBe('Email is invalid');
+            done(); 
+        });
+    });
+});
