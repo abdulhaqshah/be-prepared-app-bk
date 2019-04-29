77f9340ad4dc9e887d3362f9b21208803d844c09
Haris1994 <billichuha@hotmail.com>
2019-04-29 13:16:34 +0500

    Made changes for the PR#2

--- server/libs/userlibs.js
+++ server/libs/userlibs.js
@@ -1,113 +1,116 @@
-const User = require('../DataBase/models/user');
-const validator = require('validator');
-const _ = require('lodash');
-const bcrypt = require('bcryptjs');
-const {statusCodes, messages} = require('../utilities/constants')
+const User = require ('../DataBase/models/user');
+const validator = require ('validator');
+const _ = require ('lodash');
+const bcrypt = require ('bcryptjs');
+const {statusCodes, messages} = require ('../utilities/constants');
 
-//register a new user to the app
-exports.register = function(req,res) {
-    let body = _.pick(req.body, ['name', 'email','password']);
+exports.register = function (req,res) {
+    let body = _.pick ( req.body, ['name', 'email','password']);
     let user = new User(body);
 
-    user.save().then(() => {
+    user.save().then( () => {
         res.status(statusCodes.created).send({message : `User ${messages.created}`, data : user});
-    }).catch ((error) => {
+    }).catch ( (error) => {
         res.status(statusCodes.bad_request).send(error);
     }); 
 };
 
-//login functionality for the app. Checks whether user exist and if exist the information provided is correct
-exports.login = function(req,res){
-    let body = _.pick(req.body , ['email','password']);
+exports.login = function (req,res) {
+    let body = req.body;
     let data = {};
 
-    const validateUser = function(email,password){
-    return User.findOne({email}).then((user) => {
-        if(!user){
-            data = {
-                message : `User ${messages.not_found}`,
-                status : statusCodes.not_found
-            };
-            return Promise.reject(data);
-        }
-        return new Promise ((resolve, reject) => {
-            bcrypt.compare(password, user.password , (err,res) => {                
-                if(res){
-                    resolve(user);
-                }else{
-                    data = {
-                        message : `Password ${messages.not_match}`,
-                        status : statusCodes.forbidden
-                    };
-                    reject(data);
-                }
+    const validateUser = function (email,password) {
+        return User.findOne({email}).then((user) => {
+            if (!user) {
+                data = {
+                    message : `User ${messages.not_found}`,
+                    status : statusCodes.not_found
+                };
+                return Promise.reject(data);
+            }
+            return new Promise ( (resolve, reject) => {
+                bcrypt.compare(password, user.password , (err,res) => {                
+                    if (res) {
+                        resolve(user);
+                    } else {
+                        data = {
+                            message : `Password ${messages.not_match}`,
+                            status : statusCodes.forbidden
+                        };
+                        reject (data);
+                    }
+                })
             })
         })
-    })
 }
-    validateUser(body.email, body.password).then((user) => {
+    validateUser(body.email, body.password).then( (user) => {
         res.status(statusCodes.successful).send({message : `Login ${messages.successful}`, data : user});
     }).catch((error) => {
         res.status(error.status).send(error.message);
     });
 };
 
-exports.updateUser = function(req,res) {
+exports.updateUser = function (req,res) {
     let body = req.body;
 
-    const validationForNameExistence = (object) => {
-        if("name" in object){
-            return true;
-        }else{//eslint-disable-line
-            return false;
+    const validationForExistence = (object) => {
+        if ("name" && "email" in object) {
+            return 1;
+        } else if (!('name' in object) && "email" in object) {
+            return 2;
+        } else if (!('email' in object) && "name" in object) {
+            return 3;
         }
-    }
-
-    const validationForEmailExistence = (object) => {
-        if("email" in object){
-            return true;
-        }else{//eslint-disable-line
-            return false;
-        }
+            return 4;
+        
     }
 
     const emptyCheck = (object) => {
-        if(object.length === 0){
+        if (object.length === 0) {
             return false;
-        }else{//eslint-disable-line
-            return true;
         }
+            return true;
+        
     }
 
     const update = (object) => {
         User.findByIdAndUpdate(object.id, object, {new: true}, (err, doc) => {
-            if(doc){
+            if (doc) {
                 res.status(statusCodes.successful).send({message : `User ${messages.updated}`, data : doc})
-            }else{
+            } else {
                 res.status(statusCodes.not_found).send(`User ${messages.not_found}`);
             }
         })
     }
 
     const emailValidation = (object) => {
-        if(validationForEmailExistence(object)){
             if(validator.isEmail(object.email)){
                 update(object);
             }else{
                 res.status(statusCodes.forbidden).send(`Email ${messages.invalid}`);
             }
-        }else{
-            update(object);
-        }
     }
 
-    if(validationForNameExistence(body)){
-        if(emptyCheck(body.name)){
+    switch (validationForExistence(body)) {
+        case 1:
+            if (emptyCheck(body.name)) {
+                emailValidation(body);
+            } else {
+                res.status(statusCodes.forbidden).send(`Name ${messages.empty}`);
+            }
+            break;
+        case 2:
             emailValidation(body);
-        }else{
-            res.status(statusCodes.forbidden).send(`Name ${messages.empty}`);
-        }
-    }else{
-        emailValidation(body);
+            break;
+        case 3:
+            if (emptyCheck(body.name)) {
+                update(body);
+            }
+            break;
+        case 4:
+            update(body);
+            break;
+        default:
+            break;
     }
 };
--- server/utilities/constants.js
+++ server/utilities/constants.js
@@ -1,20 +1,20 @@
 const statusCodes = {
-    successful : "200",
-    created : "201",
+    successful  : "200",
+    created     : "201",
     bad_request : "400",
-    forbidden : "403",
-    not_found : "404"
+    forbidden   : "403",
+    not_found   : "404"
 }
 
 const messages = {
-    successful : "has been succesful",
-    created : "has been created successfully",
+    successful  : "has been succesful",
+    created     : "has been created successfully",
     bad_request : "This is is a bad request",
-    not_match : "does not match",
-    not_found : "has not been found",
-    updated : "has been updated",
-    invalid : "is invalid",
-    empty : "is empty"
+    not_match   : "does not match",
+    not_found   : "has not been found",
+    updated     : "has been updated",
+    invalid     : "is invalid",
+    empty       : "is empty"
 }
 
 module.exports = {statusCodes, messages}
