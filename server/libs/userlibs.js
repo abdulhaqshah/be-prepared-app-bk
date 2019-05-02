c3ed0c3be92e630e1eb543990f59e072da51261e
Haris1994 <billichuha@hotmail.com>
2019-05-02 12:29:16 +0500

    Made a middleware function authenticate and removed unnecessary functions in userlibs

--- server/libs/userlibs.js
+++ server/libs/userlibs.js
@@ -4,63 +4,63 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const {statusCodes, messages} = require('../utilities/constants')
 
-//register a new user to the app
 exports.register = function(req,res) {
-    let body = _.pick(req.body, ['name', 'email','password']);
-    let user = new User(body);
-
-    user.save().then(() => {
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then((user) => {
         res.status(statusCodes.created).send({message : `User ${messages.created}`, data : user});
-    }).catch ((error) => {
+    }).catch((error) => {
         res.status(statusCodes.bad_request).send(error);
-    }); 
+    });
 };
 
-//login functionality for the app. Checks whether user exist and if exist the information provided is correct
 exports.login = function(req,res){
-    let body = _.pick(req.body , ['email','password']);
+    let email = req.body.email;
+    let password = req.body.password;
+    let data = {};
+    
+    const generateToken = function (id) {
+        return User.findOne({_id: id}).then((user) => {
+           if (user) {
+               let access = 'authentication';
+               let token = jwt.sign({_id: user._id.toHexString()}, access, process.env.JWT_SECRET).toString();
+               user.tokens = user.tokens.concat([{access,token}]);
+               return user.save().then(() => token);
+           }
+       }).catch((e) => e);
+   }
 
-    const validateUser = function(email,password){
-    return User.findOne({email}).then((user) => {
-        if(!user){
-            const data = {
+    User.findOne({email}).then((user) => {
+        if(!user) {
+            data = {
                 message : `User ${messages.not_found}`,
                 status : statusCodes.not_found
             };
-            return Promise.reject(data);
+            res.status(data.status).send(data.message);
         }
-        return new Promise ((resolve, reject) => {
-            bcrypt.compare(password, user.password , (err,res) => {                
-                if(res){
-                    resolve(user);
-                } else {
-                    const data = {
-                        message : `Password ${messages.not_match}`,
-                        status : statusCodes.forbidden
-                    };
-                    reject(data);
-                    }
-                })
-            })
-        })
-    }
-
-    const generateToken = function (id) {
-         return User.findOne({_id: id}).then((user) => {
-            if (user) {
-                let access = 'authentication';
-                let token = jwt.sign({_id: user._id.toHexString()}, access, process.env.JWT_SECRET).toString();
-                user.tokens = user.tokens.concat([{access,token}]);
-                return user.save().then(() => token);
+        if(user.deleted) {
+            data = {
+                message : `User ${messages.not_exist}`,
+                status : statusCodes.not_found
+            };
+            res.status(data.status).send(data.message);
+        }
+        bcrypt.compare(password, user.password , (err,result) => {                
+            if(result) {
+                generateToken(user._id).then((token) => {
+                    res.header('x-authentication', token).status(statusCodes.successful).send({message : `Login ${messages.successful}`, data : user});
+                }).catch((error) => {
+                res.status(error.status).send(error.message);
+                });
+            } else {
+                data = {
+                    message : `Password ${messages.not_match}`,
+                    status : statusCodes.forbidden
+                };
+                res.status(data.status).send(data.message);
             }
-        }).catch((e) => e);
-    }
-
-    validateUser(body.email, body.password).then((user) => {
-        generateToken(user._id).then((token) => {
-            res.header('x-authentication', token).status(statusCodes.successful).send({message : `Login ${messages.successful}`, data : user});
-        }).catch((error) => {
-        res.status(error.status).send(error.message);
-        });
+        })
     })
 };
