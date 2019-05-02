eadcbb674cb2494a8e5a44a073428ee260e9be16
Haris1994 <billichuha@hotmail.com>
2019-05-02 19:20:21 +0500

    Made changes so that funtions become reusable

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const user_controller = require('../libs/userlibs');
+const userLibs = require('../libs/userlibs');
+const {statusCodes, messages} = require ('../utilities/constants');
 
-router.post('/user/register', user_controller.register);
-router.post('/user/login', user_controller.login);
-router.patch('/user/userUpdate', user_controller.updateUser);
+router.post('/user/register', (req, res) => {
+    if(!req.body.name) {
+        res.status(statusCodes.forbidden).send(`Name ${messages.empty}`); 
+    }
+    if(!req.body.password) {
+        res.status(statusCodes.forbidden).send(`Password ${messages.notProvided}`);
+    }
+    userLibs.register(req.body).then((user) => {
+        res.status(statusCodes.created).send({message : `User ${messages.created}`, data : user});
+    }).catch((error) => {
+        if(error.code === 11000){
+            res.status(statusCodes.badRequest).send(`Email ${messages.duplicate}`);
+        }
+        res.status(statusCodes.forbidden).send(`Email ${messages.invalid}`);  
+    });
+});
+
+router.post('/user/login', (req,res) => {
+    userLibs.login(req.body.email, req.body.password).then((user) => {
+        res.status(statusCodes.successful).send({message : `Login ${messages.successful}`, data : user});
+    }).catch((error) => {
+        res.status(error.status).send(error.message);
+    })
+});
+
+router.patch('/user/userUpdate', (req,res) => {
+    userLibs.updateUser(req.body).then((user) => {
+        res.status(statusCodes.successful).send({message : `User ${messages.updated}`, data : user});
+    }).catch((error) => {
+        res.status(error.status).send(error.message);
+    })
+});
 
 module.exports = router;
--- server/utilities/constants.js
+++ server/utilities/constants.js
@@ -1,21 +1,23 @@
 const statusCodes = {
     successful  : "200",
     created     : "201",
-    bad_request : "400",
+    badRequest : "400",
     forbidden   : "403",
-    not_found   : "404"
+    notFound   : "404"
 }
 
 const messages = {
     successful  : "has been succesful",
     created     : "has been created successfully",
-    bad_request : "This is is a bad request",
-    not_match   : "does not match",
-    not_found   : "has not been found",
+    badRequest  : "This is is a bad request",
+    notMatch    : "does not match",
+    notFound    : "has not been found",
     updated     : "has been updated",
     invalid     : "is invalid",
     empty       : "is empty",
-    not_exist   : "does not exist"
+    notExist    : "does not exist",
+    duplicate   : "already exist",
+    notProvided    : "not provided"
 }
 
 module.exports = {statusCodes, messages}
