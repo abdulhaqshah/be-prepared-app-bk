da9e33b3b95eddb2d7ab08ec2ff28beb0e2975aa
Haris1994 <billichuha@hotmail.com>
2019-05-09 11:47:29 +0500

    Indented the code and removed unnecessary functions

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -4,31 +4,29 @@ const jwt = require('jsonwebtoken');
 const {statusCodes, messages, secretKeys} = require ('../utilities/constants');
 const {isEmpty} = require('./../utilities/utilityFunctions');
 
-const findByToken = function(token,req) {
+const findByToken = function(token,id,req) {
     let decoded;
-
-    try {
-        decoded = jwt.verify(token, secretKeys.tokenKey, process.env.JWT_SECRET);
-    } catch (error) {
-        return Promise.reject(error);
-    }
-    
-    if(!isEmpty(req.params)) {
-        if(req.params.id === decoded._id) {
-            return User.findById(req.params.id);
+    return new Promise((resolve,reject) => {
+        try {
+            decoded = jwt.verify(token, secretKeys.tokenKey, process.env.JWT_SECRET);
+        } catch (error) {
+            reject(error);
         }
-        return Promise.reject({status : statusCodes.unauthorized, message : messages.unauthorized});
-    }
-    if(req.body.id === decoded._id) {
-        return User.findById(req.body.id);
-    }
-    return Promise.reject({status : statusCodes.unauthorized, message : messages.unauthorized});
+        if(id === decoded._id) {
+            resolve(User.findById(req.body.id));
+        }
+        reject({status : statusCodes.unauthorized, message : messages.unauthorized});
+    })
+    
 }
 const authenticate = (req, res, next) => {
     let token = req.header('x-authentication');
-    Token.findOne({token : 'khlkl'}).then((result) => {
-        if(!result) {
-            findByToken(token,req).then((user) => {
+    let id = req.header('id');
+    Token.findOne({token}).then((result) => {
+        if(result) {
+            res.status(statusCodes.unauthorized).send({status : statusCodes.unauthorized, message : messages.unauthorized});
+        } else {
+            findByToken(token,id,req).then((user) => {
                 if(!user) {
                     res.status(statusCodes.unauthorized).send({status : statusCodes.unauthorized, message : messages.unauthorized});
                 }
@@ -39,7 +37,6 @@ const authenticate = (req, res, next) => {
                 res.status(statusCodes.unauthorized).send({status : statusCodes.unauthorized, message : messages.unauthorized});
             });
         }
-        res.status(statusCodes.unauthorized).send({status : statusCodes.unauthorized, message : messages.unauthorized});
 
     }).catch((error) => error);
 };
